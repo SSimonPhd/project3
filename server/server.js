87fc57cb6c372641ffb168675488b69fc31ba1bf
SSimonPhd <simon_phd@protonmail.com>
2023-01-14 03:25:45 -0600

    Coded schemas, models, connection, auth

--- server/server.js
+++ server/server.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-const { authMiddleware } = require('./utils/auth');
-
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-
-const PORT = process.env.PORT || 3001;
+const { authMiddleware } = require('./utils/auth');
 const app = express();
+const PORT = process.env.PORT || 3001;
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -38,8 +37,8 @@ const startApolloServer = async (typeDefs, resolvers) => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
       console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-    })
-  })
+  	  });
+  	});
   };
   
 // Call the async function to start the server
