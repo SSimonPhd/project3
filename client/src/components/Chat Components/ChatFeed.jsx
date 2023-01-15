8c951dbe4ff1d438dbf5efb405ef40a533e034a2
mkdemon5 <alvaradomike5@gmail.com>
2023-01-15 15:25:55 -0600

    added My & Their Message components & built out chat feed placeholder

---
+++ client/src/components/Chat Components/ChatFeed.jsx
@@ -0,0 +1,13 @@
+import MessageForm from './MessageForm';
+import MyMessage from './MyMessage';
+import TheirMessage from './TheirMessage';
+
+const ChatFeed = () => {
+    return (
+        <div>
+            ChatFeed
+        </div>
+    )
+}
+
+export default ChatFeed;
