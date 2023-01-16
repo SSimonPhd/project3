9016ef68793333cfff583d9e5fddacdee68f39b0
EricaE <106550558+ericaemorabito@users.noreply.github.com>
2023-01-15 19:48:31 -0600

    basic form add trip page

--- client/src/pages/AddTrip.js
+++ client/src/pages/AddTrip.js
@@ -1,10 +1,27 @@
 import React from 'react';
 
-function AddTrip () {
+function AddTrip() {
   return (
-    <>
-    add trip
-    </>
+    <div className='container-sm'>
+      {/* Title  */}
+      <h2 className='text-center pt-3'>Add a Trip</h2>
+
+      {/* Form to Add a Trip */}
+      <div className='row'>
+        <div className='col w-50 d-flex flex-column align-items-center justify-contents-center'>
+          <div class="mb-3 w-50">
+            <label for="location" class="form-label">Location</label>
+            <input type="text" class="form-control" id="location" placeholder="Maldives" />
+          </div>
+          <div class="mb-3 w-50">
+            <label for="highlights" class="form-label">Notes</label>
+            <textarea class="form-control" id="highlights" rows="3" placeholder='Write your must see/do activities'></textarea>
+          </div>
+          {/* Add a Trip + Button */}
+          <button type="button" class="btn btn-primary p-2">Create trip +</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
