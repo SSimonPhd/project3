cf26a9cd6921b1fbd2d531499418d4895a245183
adrian brown <ninobrown58518@gmail.com>
2023-01-16 10:55:33 -0600

    Login form added

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 import axios from 'axios';
+import { Col, Button, Row, Container, Card, Form } from "react-bootstrap";
 
 const projectID = 'c324e0ea-5ec2-4823-a4d1-46eea70540a8';
 
@@ -64,7 +65,66 @@ const Login = () => {
 		}
 	};
 
-	return <div>Login Page</div>;
+	return (
+		<div>
+		  <Container>
+			<Row className="vh-100 d-flex justify-content-center align-items-center">
+			  <Col md={8} lg={6} xs={12}>
+				<div className="border border-3 border-primary"></div>
+				<Card className="shadow">
+				  <Card.Body>
+					<div className="mb-3 mt-md-4">
+					  <h2 className="fw-bold mb-2 text-uppercase ">Brand</h2>
+					  <p className=" mb-5">Please enter your login and password!</p>
+					  <div className="mb-3">
+						<Form>
+						  <Form.Group className="mb-3" controlId="formBasicEmail">
+							<Form.Label className="text-center">
+							  Email address
+							</Form.Label>
+							<Form.Control type="email" placeholder="Enter email" />
+						  </Form.Group>
+	
+						  <Form.Group
+							className="mb-3"
+							controlId="formBasicPassword"
+						  >
+							<Form.Label>Password</Form.Label>
+							<Form.Control type="password" placeholder="Password" />
+						  </Form.Group>
+						  <Form.Group
+							className="mb-3"
+							controlId="formBasicCheckbox"
+						  >
+							<p className="small">
+							  <a className="text-primary" href="#!">
+								Forgot password?
+							  </a>
+							</p>
+						  </Form.Group>
+						  <div className="d-grid">
+							<Button variant="primary" type="submit">
+							  Login
+							</Button>
+						  </div>
+						</Form>
+						<div className="mt-3">
+						  <p className="mb-0  text-center">
+							Don't have an account?{" "}
+							<a href="{''}" className="text-primary fw-bold">
+							  Sign Up
+							</a>
+						  </p>
+						</div>
+					  </div>
+					</div>
+				  </Card.Body>
+				</Card>
+			  </Col>
+			</Row>
+		  </Container>
+		</div>
+	  );
 };
 
 export default Login;
