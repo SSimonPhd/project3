9eefa95d24f85289bc98f2b1d822f1021c7e7f6c
mkdemon5 <alvaradomike5@gmail.com>
2023-01-16 19:05:50 -0600

    fixed error on MergeForm, added Chat css to App.css, plugged in chat auth to login.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
-import React from "react";
+import React from 'react';
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Header from './components/Header';
-import Footer from './components/Footer'
-import Login from "./pages/Login";
-import Chats from "./pages/Chats";
-import AddTrip from "./pages/AddTrip";
+import Footer from './components/Footer';
+import Login from './pages/Login';
+import Chats from './pages/Chats';
+import AddTrip from './pages/AddTrip';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
 // import Signup from "./pages/Signup";
@@ -14,19 +14,19 @@ import Profile from './pages/Profile';
 import './App.css';
 
 export default function App() {
-  return (
-    <Router>
-      <Header />
-      <div className="container-flex content">
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/profile' element={<Profile />} />
-          <Route path="/chats" element={<Chats />} />
-          <Route path="/addtrip" element={<AddTrip />} />
-        </Routes>
-      </div>
-      <Footer />
-    </Router>
-  );
+	return (
+		<Router>
+			<Header />
+			<div className='container-flex content'>
+				<Routes>
+					<Route path='/' element={<Home />} />
+					<Route path='/login' element={<Login />} />
+					<Route path='/profile' element={<Profile />} />
+					<Route path='/chats' element={<Chats />} />
+					<Route path='/addtrip' element={<AddTrip />} />
+				</Routes>
+			</div>
+			<Footer />
+		</Router>
+	);
 }
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 import axios from 'axios';
-import { Col, Button, Row, Container, Card, Form } from "react-bootstrap";
+import { Col, Button, Row, Container, Card, Form } from 'react-bootstrap';
 import Particle from '../tsParticles/tsParticle';
 // import Particles from "react-tsparticles";
 // import { loadFull } from "tsparticles";
@@ -43,11 +43,11 @@ const Login = () => {
 		const { username, password, email, firstName, lastName } = props;
 
 		let data = `{
-      "username": ${username},
-      "secret": ${password},
-      "email": ${email},
-      "first_name": ${firstName},
-      "last_name": ${lastName}'
+	"username": ${username},
+    "secret": ${password},
+    "email": ${email},
+    "first_name": ${firstName},
+    "last_name": ${lastName}'
     }`;
 
 		const config = {
@@ -71,65 +71,79 @@ const Login = () => {
 	return (
 		<div>
 			<Particle />
-		  <Container>		  
-			<Row className="vh-100 d-flex justify-content-center align-items-center">
-			  <Col md={8} lg={6} xs={12}>
-				<div className="border border-3 border-primary"></div>
-				<Card className="shadow">
-				  <Card.Body>
-					<div className="mb-3 mt-md-4">
-					  <h2 className="fw-bold mb-2 text-uppercase ">©The Essentials </h2>
-					  <p className=" mb-5">Please enter your login and password!</p>
-					  <div className="mb-3">
-						<Form>
-						  <Form.Group className="mb-3" controlId="formBasicEmail">
-							<Form.Label className="text-center">
-							  Email address
-							</Form.Label>
-							<Form.Control type="email" placeholder="Enter email" />
-						  </Form.Group>
-	
-						  <Form.Group
-							className="mb-3"
-							controlId="formBasicPassword"
-						  >
-							<Form.Label>Password</Form.Label>
-							<Form.Control type="password" placeholder="Password" />
-						  </Form.Group>
-						  <Form.Group
-							className="mb-3"
-							controlId="formBasicCheckbox"
-						  >
-							<p className="small">
-							  <a className="text-primary" href="#!">
-								Forgot password?
-							  </a>
-							</p>
-						  </Form.Group>
-						  <div className="d-grid">
-							<Button variant="primary" type="submit">
-							  Login
-							</Button>
-						  </div>
-						</Form>
-						<div className="mt-3">
-						  <p className="mb-0  text-center">
-							Don't have an account?{" "}
-							<a href="{''}" className="text-primary fw-bold">
-							  Sign Up
-							</a>
-						  </p>
-						</div>
-					  </div>
-					</div>
-				  </Card.Body>
-				</Card>
-			  </Col>
-			</Row>
-		  </Container>
-		  
+			<Container>
+				<Row className='vh-100 d-flex justify-content-center align-items-center'>
+					<Col md={8} lg={6} xs={12}>
+						<div className='border border-3 border-primary'></div>
+						<Card className='shadow'>
+							<Card.Body>
+								<div className='mb-3 mt-md-4'>
+									<h2 className='fw-bold mb-2 text-uppercase '>
+										©The Essentials{' '}
+									</h2>
+									<p className=' mb-5'>Please enter your login and password!</p>
+									<div className='mb-3'>
+										<Form onSubmit={handleSubmit}>
+											<Form.Group className='mb-3' controlId='formBasicEmail'>
+												<Form.Label className='text-center'>
+													Email address
+												</Form.Label>
+												<Form.Control
+													type='email'
+													placeholder='Enter email'
+													value={username}
+													onChange={(e) => setUsername(e.target.value)}
+													required
+												/>
+											</Form.Group>
+
+											<Form.Group
+												className='mb-3'
+												controlId='formBasicPassword'
+											>
+												<Form.Label>Password</Form.Label>
+												<Form.Control
+													type='password'
+													placeholder='Password'
+													value={password}
+													onChange={(e) => setPassword(e.target.value)}
+													required
+												/>
+											</Form.Group>
+											<Form.Group
+												className='mb-3'
+												controlId='formBasicCheckbox'
+											>
+												<p className='small'>
+													<a className='text-primary' href='#!'>
+														Forgot password?
+													</a>
+												</p>
+											</Form.Group>
+											<div className='d-grid'>
+												<Button variant='primary' type='submit'>
+													Login
+												</Button>
+											</div>
+											<h2 className='error'>{error}</h2>
+										</Form>
+										<div className='mt-3'>
+											<p className='mb-0  text-center'>
+												Don't have an account?{' '}
+												<a href="{''}" className='text-primary fw-bold'>
+													Sign Up
+												</a>
+											</p>
+										</div>
+									</div>
+								</div>
+							</Card.Body>
+						</Card>
+					</Col>
+				</Row>
+			</Container>
 		</div>
-	  );
+	);
 };
 
 export default Login;
