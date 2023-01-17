bc3a072a51797017ac1477d742eb4b904699436c
Catherine Polk <cpolknytx@gmail.com>
2023-01-16 18:26:45 -0600

    added responsiveness and footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,13 +1,16 @@
-// import React from 'react';
+import React from 'react';
+import './footer.scss'
 
-// //links for each of the element in the footer with bootstrap icons 
-// function Footer () {
-//   return (
-//     <div>
-//        Footer
-//     </div>
+//links for each of the element in the footer with bootstrap icons 
+function Footer () {
+  return (
+    <footer className="container-fluid footer">
+      <div className="row justify-content-center p-3 h-50">
+        Copyright © 2023 The Essentials
+      </div>
+    </footer>
    
-//   )
-// }
+  )
+}
 
-// export default Footer;  
+export default Footer;  
