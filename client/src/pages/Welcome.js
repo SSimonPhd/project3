58c8de5007e4eb6338acb32c5931527952385d1b
EricaE <106550558+ericaemorabito@users.noreply.github.com>
2023-01-18 11:43:10 -0600

    welcome page styling

--- client/src/pages/Welcome.js
+++ client/src/pages/Welcome.js
@@ -4,15 +4,17 @@ import './styles/welcome.scss';
 function Welcome() {
   return (
     <div>
-      <div className="world-background container bg-image">
+      <div className="world-background container-fluid bg-image">
         <div className="row w-100 h-100">
-          <div className="col-sm h-100 d-flex flex-column justify-content-center align-items-center">
-            <h2 className='text-white'>Welcome</h2>
+          <div className="col-sm h-100 d-flex flex-column justify-content-center align-items-center ml-5">
+            <h2 className='page-title text-white'>WELCOME</h2>
             <p className='text-white'>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            <button type="button" class="green-btn btn-outline-light btn-lg m-2 p-2">EXPLORE CHATS</button>
+            <button type="button" class="green-btn btn-outline-light btn-lg m-2 p-2">ADD A TRIP</button>
           </div>
           <div className="col-sm d-flex flex-column justify-content-center align-items-center">
-            <button type="button" class="btn btn-primary m-2">Explore chats</button>
-            <button type="button" class="btn btn-primary m-2">Add a trip +</button>
+            {/* <button type="button" class="green-btn btn-outline-light btn-lg m-2 p-2">Explore chats</button>
+            <button type="button" class="green-btn btn-outline-light btn-lg m-2 p-2">Add a trip</button> */}
           </div>
         </div>
       </div>
