0ca159b0d10cc507d0de3347306a56bd5c94cf2d
SSimonPhd <simon_phd@protonmail.com>
2023-01-23 19:24:24 -0600

    Updated seed files and deleted localStorage.js

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,7 +1,9 @@
 const db = require('../config/connection');
 const { User } = require('../models/User');
+const { Trip } = require('../models/Trip');
 
 const userData = require('./userData.json');
+const tripData = require('./tripData.json');
 
 db.once('open', async() => {
   await User.deleteMany({});
@@ -12,3 +14,11 @@ db.once('open', async() => {
   process.exit(0);
 })
 
+db.once('open', async() => {
+  await Trip.deleteMany({});
+
+  const trips = await Trip.insertMany(tripData);
+
+  console.log('Trips are seeded.');
+  process.exit(0);
+})
